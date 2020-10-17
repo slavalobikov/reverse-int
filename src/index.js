3de277dcb0e7d38d8414b5f3784774ed042d8808
lobik <lobikova12@mail.ru>
2020-10-17 22:17:24 +0300

    fix: add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n = String(Math.abs(n));
+    n = n.split('');
+    let sub = [];
+    let res = '';
+    for(let i = 0; i < n.length; i++) {
+        sub[i] = n[n.length-1-i];
+        res += sub[i]
+    }
+    return Number(res);
 }
